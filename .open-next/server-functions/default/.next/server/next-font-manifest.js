91a7773ffd6591251a8ff993c7b314e821ab5a4c
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-11-22 20:43:37 -0300

    Remove .open-next from tracking

--- .open-next/server-functions/default/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/_not-found/page\": [\n      \"static/media/f06bf9da926bae75-s.p.c68a79fb.woff2\"\n    ],\n    \"[project]/app/page\": [\n      \"static/media/f06bf9da926bae75-s.p.c68a79fb.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"P0wrF3G6u3NsTlpKRaNdC3H98qbWd3hGWkmCyiOcQT8=\"\n}"
