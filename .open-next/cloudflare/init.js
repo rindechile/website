667a92a69479fcaf7a34c0fcd17f2a7c21a696f9
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-11-22 20:17:55 -0300

    fix: swap global error and not found page client reference manifests in handler.mjs.meta.json; update NextConfig and BuildId in index.mjs

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1763851563953,
+    __BUILD_TIMESTAMP_MS__: 1763853071473,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
